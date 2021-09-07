d8e1ca1e19a88cb4f3112ac2a27c752e29baed3a
jhmartins1 <jhmartinsrdo@gmail.com>
2021-09-07 18:36:07 -0300

    configs

--- repository/Conexao.php
+++ repository/Conexao.php
@@ -2,6 +2,17 @@
 
 class Conexao{
   public static function criar():PDO{
-    return new PDO('sqlite:database/filmes.db');
+    $env = parse_ini_file('.env');
+    $server = $env["server"];
+    $database = $env["database"];
+    $databaseType = $env["databasetype"];
+    $user = $env["user"];
+    $password = $env["password"];
+
+    if($databaseType === "mysql"){
+      $database = "host=$server;dbname=$database";
+    }
+
+    return new PDO("$databaseType:$database", $user, $password);
   }
 }
